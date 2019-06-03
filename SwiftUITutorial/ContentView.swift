f307bc21b6c209f94751366ab50b917ee85f61d5
Yuki <toxicelda@gmail.com>
2019-06-04 06:42:37 +0900

    Initial Commit

---
+++ SwiftUITutorial/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Yuki on 2019/06/04.
+//  Copyright © 2019 418. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
